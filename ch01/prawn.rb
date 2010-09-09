e7a5d2cd776e0592f601b79036ff3af3db8846d3
Enrique Temes <enrtemes@gmail.com>
2010-09-08 23:11:48 -0400

    refactor parse_inline_styles

--- ch01/prawn.rb
+++ ch01/prawn.rb
@@ -1,25 +1,8 @@
 module Prawn
   class Document
     def parse_inline_styles(text)
-      require 'strscan'
-    
-      sc = StringScanner.new(text)
-      output = []
-      last_pos = 0
-
-      loop do
-        if sc.scan_until(/<\/?[ib]>/)
-          pre = sc.pre_match[last_pos..-1]
-          output << pre unless pre.empty?
-          output << sc.matched
-          last_pos = sc.pos
-        else
-          output << sc.rest if sc.rest?
-          break output
-        end
-      end
-
-      output.length == 1 ? output.first : output
+      segments = text.split( %r{(</?.*?>)} ).reject { |x| x.empty? }
+      segments.size == 1 ? segments.first : segments
     end
   end
 end
