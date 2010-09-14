980e2506cfe63a7fe32269ef4c4da85d926d880b
Enrique Temes <enrtemes@gmail.com>
2010-09-13 23:21:15 -0400

    questioner tests with StringIO class

--- ch01/questioner.rb
+++ ch01/questioner.rb
@@ -1,21 +1,21 @@
 class Questioner
 
-  def inquire_about_happiness
-    ask("Are you happy?") ? "Good I'm Glad" : "That's Too Bad"
+  def initialize(input=STDIN, output=STDOUT)
+    @input = input
+    @output = output
   end
 
   def ask(question)
-    puts question
-    response = yes_or_no(gets.chomp)
-    response.nil? ? ask(question) : response
+    @output.puts question
+    response = @input.gets.chomp
+    case(response)
+    when /^y(es)?$/i
+      true
+    when /^no?$/i
+      false
+    else
+      @output.puts "I don't understand."
+      ask question
+    end
   end
-
-   def yes_or_no(response)
-     case(response)
-     when /^y(es)?$/i
-       true
-     when /^no?$/i
-       false
-     end
-   end
 end
--- test/test_questioner.rb
+++ test/test_questioner.rb
@@ -1,39 +1,48 @@
 require 'minitest/unit'
-require 'flexmock/test_unit'
 require '/home/enrique/rubys/book_code/rbp/test_unit_extensions'
 require '/home/enrique/rubys/book_code/rbp/ch01/questioner'
+require 'stringio'
 
 class QuestionerTest < Test::Unit::TestCase
 
   def setup
-    @questioner = Questioner.new
+    @input = StringIO.new
+    @output = StringIO.new
+    @questioner = Questioner.new(@input, @output)
+    @question = "Are you happy?"
   end
 
-  must "respond 'Good I'm Glad' when inquire_about_happiness gets 'yes'" do
-    stubbed = flexmock(@questioner, :ask => true)
-    assert_equal "Good I'm Glad", stubbed.inquire_about_happiness
-  end
-
-  must "respond 'That's Too Bad' when inquire_about_happiness gets 'no'" do
-    stubbed = flexmock(@questioner, :ask => false)
-    assert_equal "That's Too Bad", stubbed.inquire_about_happiness
-  end
-
-  %w[y Y YeS YES yes].each do |yes|
-    must "return true when yes_or_no parses #{yes}" do
-      assert @questioner.yes_or_no(yes), "#{yes.inspect} expected to parse as true"
+  ["y", "Y", "YeS", "YES", "yes"].each do |y|
+    must "return true when parsing #{y}" do
+      provide_input(y)
+      assert @questioner.ask(@question), "Expected '#{y}' to be true"
+      expect_output "#{@question}\n" 
     end
   end
 
   %w[n N no nO].each do |no|
-    must "return false when yes_or_no parses #{no}" do
-      assert ! @questioner.yes_or_no(no), "#{no.inspect} expected to parse as false"
+    must "return false when parsing #{no}" do
+      provide_input(no)
+      assert ! @questioner.ask(@question)
+      expect_output "#{@question}\n"
     end
   end
 
-  %w[Note Yesterday xyzaty].each do |mu|
-    must "return nil because #{mu} is not a variant of 'yes' or 'no'" do
-      assert_nil @questioner.yes_or_no(mu), "#{mu.inspect} expected to parse as nil"
+  [["y", true], ["n", false]].each do |input, state| 
+    must "continue to ask for input until given #{input}" do
+      provide_input "Note\nYesterday\nxyzaty\n#{input}"
+      assert_equal state, @questioner.ask(@question)
+      expect_output "#{@question}\nI don't understand.\n"*3 + "#{@question}\n"
     end
   end
+
+  def provide_input(string)
+    @input << string
+    @input.rewind
+  end
+
+  def expect_output(string)
+    assert_equal string, @output.string
+  end
 end
+
