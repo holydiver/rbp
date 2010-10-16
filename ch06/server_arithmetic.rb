c4f3219650ab976268080b1a64b805f13e77e095
Enrique Temes <enrtemes@gmail.com>
2010-10-16 09:48:40 -0400

    server/client with no logging support

---
+++ ch06/server_arithmetic.rb
@@ -0,0 +1,34 @@
+require 'socket'
+
+class Server
+
+  def initialize
+    @server = TCPServer.new('localhost', port=3333)
+  end
+
+  def *(x, y)
+    "#{Float(x) * Float(y)}"
+  end
+
+  def /(x, y)
+    "#{Float(x) / Float(y)}"
+  end
+
+  def handle_request(session)
+    action, *args = session.gets.split(/\s/)
+    if ["*", "/"].include?(action)
+      session.puts(send(action, *args))
+    else
+      session.puts("Invalid command")
+    end
+  end
+
+  def run
+    while session = @server.accept
+      handle_request(session)
+    end
+  end
+end
+
+server = Server.new
+server.run
